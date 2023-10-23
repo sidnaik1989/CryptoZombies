ee56b7145b52821eb8e666e1b61037ec8d4e2f2d
siddhesh <sidnaik1989@gmail.com>
2023-10-23 13:52:59 +0530

    uncomment code

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -8,22 +8,22 @@ pragma solidity >=0.8.2 <0.9.0;
 contract ZombieFactory {
 
     // chapter 13: Events. Define event to be emitted on Zombie creation
-    // event NewZombie(uint zombieId, string name, uint dna);
+    event NewZombie(uint zombieId, string name, uint dna);
 
     // chapter 3: State variables. Number of zombies
-    //uint public zombiesCreated = 0;
+    uint public zombiesCreated = 0;
 
     // chapter 4: Structs. Defining a zombie
-    /*struct Zombie {
+    struct Zombie {
         string name;  // name of the zombie
         uint dna;     // 16 digit DNA of the zombie 
-    }*/
+    }
 
     // chapter 6: Arrays
-    //Zombie[] public zombies;
+    Zombie[] public zombies;
 
     // chapter 7: Function declarations. Create a public function to create a zombie
-    /*function createZombie(string calldata _name) public returns (uint) {
+    function createZombie(string calldata _name) public returns (uint) {
         uint _dna = 1000000000000000;    // chapter 8: call private function. _dna = _generateDna(_name)
 
         // create a zombie and add to array
@@ -33,7 +33,7 @@ contract ZombieFactory {
         // increment count of zombies created
         zombiesCreated = zombiesCreated + 1;
         return zombiesCreated;
-    }*/
+    }
 
     //chapter 8: Private functions. Create a DNA for the zombie based on the name
     /*function _generateDna(string memory _str) private pure returns (uint) {
